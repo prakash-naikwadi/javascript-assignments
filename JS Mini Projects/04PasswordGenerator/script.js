e4ca0571738e2377e5e8cb526acda991b878aaf6
prakash naikwadi <naikwadiprakash1@gmail.com>
2022-09-26 12:43:29 +0530

    dom assignments added

---
+++ JS Mini Projects/04PasswordGenerator/script.js
@@ -0,0 +1,39 @@
+const resultEl = document.getElementById('result')
+const lengthEl = document.getElementById('length')
+const uppercaseEl = document.getElementById('uppercase')
+const lowercaseEl = document.getElementById('lowercase')
+const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+const randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+}
+
+clipboardEl.addEventListener('click', () => {
+    
+})
+
+generateEl.addEventListener('click', () => {
+    
+})
+
+function generatePassword(lower, upper, number, symbol, length) {
+    
+}
+
+function getRandomLower() {
+}
+
+function getRandomUpper() {
+}
+
+function getRandomNumber() {
+}
+
+function getRandomSymbol() {
+}
