f7ae73e7f78a8a17794a67ab661d12018297255c
prakash naikwadi <naikwadiprakash1@gmail.com>
2022-10-04 18:34:16 +0530

    calculator app functionality added and css

---
+++ javascript-projects-word-file/Calculator App/app.js
@@ -0,0 +1,56 @@
+const numberButtons = document.querySelectorAll(".number-btn");
+const operatorsButtons = document.querySelectorAll(".operator-btn");
+
+const equalsOperator = document.getElementById("equal");
+const dotButton = document.querySelector(".dot-btn");
+const clearButton = document.querySelector(".clear-btn");
+
+const displayScreen = document.querySelector(".screen");
+
+const numberButtonsArray = Array.from(numberButtons);
+const operatorButtonsArray = Array.from(operatorsButtons);
+
+let clearErrorFlag = false;
+
+// console.log(equalsOperator);
+
+numberButtonsArray.map((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+
+    if (clearErrorFlag) {
+      displayScreen.innerText = "";
+      clearErrorFlag = false;
+    }
+
+    displayScreen.innerText += e.target.innerText;
+  });
+});
+
+operatorButtonsArray.map((item) => {
+  item.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+    displayScreen.innerText += e.target.innerText;
+  });
+});
+
+dotButton.addEventListener("click", (e) => {
+  displayScreen.innerText += e.target.innerText;
+});
+
+clearButton.addEventListener("click", (e) => {
+  displayScreen.innerText = "";
+});
+
+equalsOperator.addEventListener("click", (e) => {
+  console.log(e.target.innerText);
+  // console.log(eval(displayScreen.innerText.trim()));
+  try {
+    if (eval(displayScreen.innerText.trim())) {
+      displayScreen.innerText = eval(displayScreen.innerText.trim());
+    }
+  } catch {
+    displayScreen.innerText = "Error!";
+    clearErrorFlag = true;
+  }
+});
